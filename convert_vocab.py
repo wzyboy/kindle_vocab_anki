a343927cf34461e030c498a557071dbf73ef2a52
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-05-27 23:53:51 +0800

    Look up definition and output Anki tsv

--- convert_vocab.py
+++ convert_vocab.py
@@ -2,7 +2,14 @@
 
 import sqlite3
 import argparse
+import itertools
 from datetime import datetime
+from operator import itemgetter
+from operator import attrgetter
+from collections import namedtuple
+
+
+AnkiNote = namedtuple('AnkiNote', 'word usage definition timestamp')
 
 
 def pbar(iterable):
@@ -31,18 +38,67 @@ def get_vocab(vocab_db, _since=0):
         left join BOOK_INFO
         on BOOK_INFO.id = LOOKUPS.book_key
         where LOOKUPS.timestamp > ?
-        order by WORDS.stem
+        order by WORDS.stem, LOOKUPS.timestamp
     '''
     rows = cur.execute(sql, (since,)).fetchall()
     return rows
+
+
+def make_notes(vocab, dict_tsv):
+
+    # OALD is ~38 MiB for each stem and ~110 MiB for each iform,
+    # read it all into a Python dict does not hurt on mordern PCs.
+    with open(dict_tsv, 'r') as f:
+        dict_db = dict(line.split('\t') for line in f.readlines())
+
+    stems_no_def = set()
+    notes = []
+    # vocab is a list of db rows order by (stem, timestamp)
+    for stem, entries in itertools.groupby(vocab, itemgetter('stem')):
+        # Merge multiple usages into one.
+        usage_all = ''
+        usage_timestamp = None
+        for entry in entries:
+            word = entry['word']
+            _usage = entry['usage'].replace(word, f'<strong>{word}</strong>').strip()
+            usage = f'<blockquote>{_usage}<small>{entry["title"]}</small></blockquote><br/>'
+            usage_all += usage
+            usage_timestamp = entry['timestamp']
+
+        # Look up definition in dictionary
+        try:
+            definition = dict_db[stem].strip()
+        except KeyError:
+            stems_no_def.add(stem)
+            definition = None
+
+        note = AnkiNote(stem, usage_all, definition, usage_timestamp)
+        notes.append(note)
+
+    if stems_no_def:
+        print(f'WARNING: Some words cannot be found in dictionary:\n{stems_no_def}')
+
+    return notes
+
+
+def output_anki_tsv(notes, anki_tsv, sort=True):
+
+    if sort:
+        notes.sort(key=attrgetter('timestamp'), reverse=True)
+
+    with open(anki_tsv, 'w') as f:
+        for note in pbar(notes):
+            line = f'{note.word}\t{note.usage}\t{note.definition}\n'
+            f.write(line)
 
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
     argp.add_argument('--since', type=lambda s: datetime.strptime(s, '%Y-%m-%d'))
     argp.add_argument('vocab_db')
+    argp.add_argument('dict_tsv')
     argp.add_argument('anki_tsv')
     args = argp.parse_args()
     vocab = get_vocab(args.vocab_db)
-    for v in vocab:
-        print(datetime.utcfromtimestamp(v['timestamp'] / 1000), v['stem'], v['usage'], v['title'])
+    notes = make_notes(vocab, args.dict_tsv)
+    output_anki_tsv(notes, args.anki_tsv)
