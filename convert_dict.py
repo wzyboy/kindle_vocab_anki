805b37c299aa70c12aa6f00a095c83de8b970888
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-05-28 01:30:53 +0800

    --expand-iform

--- convert_dict.py
+++ convert_dict.py
@@ -6,14 +6,14 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
-def main(book, dic):
+def main(dict_html, dict_tsv, expand_iform=False):
 
-    with open(book, 'r') as f:
+    with open(dict_html, 'r') as f:
         html = f.read()
     print('Parsing markup data, this may take a while ...')
     soup = BeautifulSoup(html, 'xml')
 
-    with open(dic, 'w') as f:
+    with open(dict_tsv, 'w') as f:
         for entry in tqdm(soup.html.body.contents):
             if entry.name != 'entry':
                 continue
@@ -25,13 +25,15 @@ def main(book, dic):
                 if tag.name != 'a'  # anchors do not work in Anki, strip them
             )
             f.write(f'{stem}\t{definition}\n')
-            for iform in iforms:
-                f.write(f'{iform}\t{definition}\n')
+            if expand_iform:
+                for iform in iforms:
+                    f.write(f'{iform}\t{definition}\n')
 
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
     argp.add_argument('dict_html')
     argp.add_argument('dict_tsv')
+    argp.add_argument('--expand-iform', action='store_true')
     args = argp.parse_args()
-    main(args.dict_html, args.dict_tsv)
+    main(args.dict_html, args.dict_tsv, args.expand_iform)
