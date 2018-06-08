0f7dbc06d5d1fd90ddf5440119d8b8f89e1c44f8
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-06-08 04:01:40 -0400

    convert_dict: allow custom seperator

--- convert_dict.py
+++ convert_dict.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 
 
-def main(dict_html, dict_tsv, expand_iform=False):
+def main(dict_html, dict_tsv, expand_iform=False, sep='\t'):
 
     print('Parsing markup data, this may take a while ...')
     parse_only_entry = SoupStrainer('entry')
@@ -24,10 +24,10 @@ def main(dict_html, dict_tsv, expand_iform=False):
                 str(tag) for tag in _definition
                 if tag.name != 'a'  # anchors do not work in Anki, strip them
             ).strip()
-            f.write(f'{stem}\t{definition}\n')
+            f.write(f'{stem}{sep}{definition}\n')
             if expand_iform:
                 for iform in iforms:
-                    f.write(f'{iform}\t{definition}\n')
+                    f.write(f'{iform}{sep}{definition}\n')
 
 
 if __name__ == '__main__':
@@ -35,5 +35,10 @@ if __name__ == '__main__':
     argp.add_argument('dict_html')
     argp.add_argument('dict_tsv')
     argp.add_argument('--expand-iform', action='store_true')
+    argp.add_argument('--seperator', default='\t')
     args = argp.parse_args()
-    main(args.dict_html, args.dict_tsv, args.expand_iform)
+    main(
+        args.dict_html, args.dict_tsv,
+        expand_iform=args.expand_iform,
+        sep=args.seperator
+    )
