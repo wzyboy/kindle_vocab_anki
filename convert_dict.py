d0a189b6a7f0269c014b394a6dafdf6f16da6bbf
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-10-10 09:33:29 -0400

    Try to handle dictionaries in issue #2
    The definitions in these dictionaries are direct children of the <body>
    element, instead of being enclosed by a pair of <idx:entry> tags.

--- convert_dict.py
+++ convert_dict.py
@@ -5,27 +5,32 @@ from ast import literal_eval
 
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
 
 
 def main(dict_html, dict_tsv, expand_iform=False, sep='\t'):
 
     print('Parsing markup data, this may take a while ...')
-    parse_only_entry = SoupStrainer('entry')
     with open(dict_html, 'r', encoding='utf-8') as f:
-        soup = BeautifulSoup(f, 'xml', parse_only=parse_only_entry)
+        soup = BeautifulSoup(f, 'lxml-html')
 
     print('Converting HTML dictionary into TSV dictionary ...')
     with open(dict_tsv, 'w', encoding='utf-8') as f:
-        for entry in tqdm(soup.find_all('entry')):
-            orth = entry.find('orth')
-            _definition = orth.next_siblings
+        for entry in tqdm(soup.find_all('idx:entry')):
+
+            orth = entry.find('idx:orth')
             stem = orth['value']
-            iforms = [i['value'] for i in orth.find_all('iform')]
+            iforms = [i['value'] for i in orth.find_all('idx:iform')]
+
+            _definition = []
+            for n in entry.next_siblings:
+                if n.name == 'hr':
+                    break
+                _definition.append(n)
             definition = ''.join(
                 str(tag) for tag in _definition
                 if tag.name != 'a'  # anchors do not work in Anki, strip them
             ).strip()
+
             f.write(f'{stem}{sep}{definition}\n')
             if expand_iform:
                 for iform in iforms:
