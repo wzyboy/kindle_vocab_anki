e03bc912aaf588fa6d694b7274c462ad0fcedaba
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-10-11 03:36:36 -0400

    Expand Kanjis in Japanese dictionaries

--- convert_dict.py
+++ convert_dict.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import argparse
 from ast import literal_eval
 
@@ -7,7 +8,7 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 
-def main(dict_html, dict_tsv, expand_iform=False, sep='\t'):
+def main(dict_html, dict_tsv, expand_iform=False, expand_kanji=False, sep='\t'):
 
     print('Parsing markup data, this may take a while ...')
     with open(dict_html, 'r', encoding='utf-8') as f:
@@ -26,15 +27,26 @@ def main(dict_html, dict_tsv, expand_iform=False, sep='\t'):
                 if n.name == 'hr':
                     break
                 _definition.append(n)
+
             definition = ''.join(
                 str(tag) for tag in _definition
                 if tag.name != 'a'  # anchors do not work in Anki, strip them
             ).strip()
 
+            # XXX: Search for and expand kanjis
+            match_kanji = re.search('【(.*)】', definition)
+            if match_kanji:
+                kanjis = match_kanji.group(1).strip().split('、')
+            else:
+                kanjis = []
+
             f.write(f'{stem}{sep}{definition}\n')
             if expand_iform:
                 for iform in iforms:
                     f.write(f'{iform}{sep}{definition}\n')
+            if expand_kanji:
+                for kanji in kanjis:
+                    f.write(f'{kanji}{sep}{definition}\n')
 
 
 if __name__ == '__main__':
@@ -42,11 +54,13 @@ if __name__ == '__main__':
     argp.add_argument('dict_html')
     argp.add_argument('dict_tsv')
     argp.add_argument('--expand-iform', action='store_true')
+    argp.add_argument('--expand-kanji', action='store_true')
     argp.add_argument('--seperator', type=lambda x: literal_eval(f"'''{x}'''"), default='\t')
     args = argp.parse_args()
 
     main(
         args.dict_html, args.dict_tsv,
         expand_iform=args.expand_iform,
+        expand_kanji=args.expand_kanji,
         sep=args.seperator
     )
